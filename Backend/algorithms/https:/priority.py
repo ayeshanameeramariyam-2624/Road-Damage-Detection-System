ff4375e9c37f11b6bcc9a1f1b925d9d1de3622b4
insharamujeebin-web <insharamujeebin@gmail.com>
2026-09-15 12:03:53 +0530

    Delete Backend/algorithms/priority.py

--- Backend/algorithms/https:/priority.py
+++
@@ -1,9 +0,0 @@
-def calculate_priority(severity):
-    severity = severity.lower()
-
-    if severity == "high":
-        return "High"
-    elif severity == "medium":
-        return "Medium"
-    else:
-        return "Low"
