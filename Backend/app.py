d781cbe0d0f3bf2283923e89ee26f86f5725142e
insharamujeebin-web <insharamujeebin@gmail.com>
2026-09-15 16:17:12 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,16 +1,13 @@
-from flask import Flask, jsonify
-from controllers.damage_controller import get_damage_data
+from flask import Flask
+from controllers.damage_controller import damage_controller
 
 app = Flask(__name__)
+
+app.register_blueprint(damage_controller)
 
 @app.route("/")
 def home():
     return "AI Road Damage Detection Backend Running!"
-
-@app.route("/damage", methods=["GET"])
-def damage():
-    data = get_damage_data()
-    return jsonify(data), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
