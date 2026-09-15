fc515a63709eb7ee5e9b87562030ef0231aa265c
insharamujeebin-web <insharamujeebin@gmail.com>
2026-09-15 13:26:23 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,39 +1,16 @@
 from flask import Flask, jsonify
-import sqlite3
+from controllers.damage_controller import get_damage_data
 
 app = Flask(__name__)
 
-def get_connection():
-    return sqlite3.connect("road_damage.db")
-
 @app.route("/")
 def home():
-    return "Road Damage Detection Backend is Running!"
+    return "AI Road Damage Detection Backend Running!"
 
-@app.route("/damage")
-def get_damage():
-    connection = get_connection()
-    cursor = connection.cursor()
-
-    cursor.execute("SELECT * FROM damage")
-    rows = cursor.fetchall()
-
-    connection.close()
-
-    damage_list = []
-
-    for row in rows:
-        damage_list.append({
-            "id": row[0],
-            "damage_type": row[1],
-            "severity": row[2],
-            "latitude": row[3],
-            "longitude": row[4],
-            "priority": row[5],
-            "estimated_cost": row[6]
-        })
-
-    return jsonify(damage_list)
+@app.route("/damage", methods=["GET"])
+def damage():
+    data = get_damage_data()
+    return jsonify(data), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
