632c2d192d0f1fcb2200d1a5079b75aa5c652b2e
insharamujeebin-web <insharamujeebin@gmail.com>
2026-09-15 12:18:16 +0530

    Delete Backend/Models directory

--- Backend/Models/Database.py
+++
@@ -1,9 +0,0 @@
-from flask import g
-import sqlite3
-
-DATABASE = "road_damage.db"
-
-def get_connection():
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
