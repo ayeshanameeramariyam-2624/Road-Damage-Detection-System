7fca00af7035822496e8ddadbf58c3330e5c660b
durgagoswami9170-spec <durgagoswami9170@gmail.com>
2026-09-23 12:10:15 +0530

    Update script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,20 +1,58 @@
-const fileInput = document.getElementById("imageInput");
-const detectBtn = document.getElementById("detectBtn");
-const resultDiv = document.getElementById("result");
+const table = document.getElementById("damageTable");
 
-fileInput.addEventListener("change", function () {
-    if (fileInput.files.length > 0) {
-        resultDiv.innerHTML = "Image selected successfully.";
-    } else {
-        resultDiv.innerHTML = "";
-    }
-});
+const totalDamage = document.getElementById("totalDamage");
+const potholes = document.getElementById("potholes");
+const highPriority = document.getElementById("highPriority");
+const totalCost = document.getElementById("totalCost");
 
-detectBtn.addEventListener("click", function () {
-    if (fileInput.files.length === 0) {
-        resultDiv.innerHTML = "Please select a road image first.";
-        return;
-    }
+fetch("http://127.0.0.1:5000/damage")
+    .then(response => response.json())
+    .then(data => {
 
-    resultDiv.innerHTML = "Image selected. Ready for detection.";
-});
+        totalDamage.textContent = data.length;
+
+        let potholeCount = 0;
+        let highCount = 0;
+        let cost = 0;
+
+        table.innerHTML = "";
+
+        data.forEach(item => {
+
+            if (item.damage_type.toLowerCase() === "pothole") {
+                potholeCount++;
+            }
+
+            if (item.priority.toLowerCase() === "high") {
+                highCount++;
+            }
+
+            cost += Number(item.estimated_cost);
+
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+                <td>${item.id}</td>
+                <td>${item.damage_type}</td>
+                <td>${item.severity}</td>
+                <td>${item.latitude}</td>
+                <td>${item.longitude}</td>
+                <td>${item.priority}</td>
+                <td>₹${item.estimated_cost}</td>
+            `;
+
+            table.appendChild(row);
+        });
+
+        potholes.textContent = potholeCount;
+        highPriority.textContent = highCount;
+        totalCost.textContent = "₹" + cost;
+    })
+    .catch(error => {
+        console.error(error);
+        table.innerHTML = `
+            <tr>
+                <td colspan="7">Backend data not available</td>
+            </tr>
+        `;
+    });
